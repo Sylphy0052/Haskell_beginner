0a8fbc042acc619349afa35de8b350d8db9f3cd2
Sylphy0052 <ma17099@shibaura-it.ac.jp>
2018-05-31 23:15:25 +0900

    add: mix color sample

---
+++ qiita_haskell_primer/chap02/Mix2.hs
@@ -0,0 +1,10 @@
+import Data.Bits
+
+data Color = Black | Blue | Red | Magenta | Green | Cyan | Yellow | White
+    deriving (Show, Enum)
+
+mix c1 c2 = toEnum (fromEnum c1 .|. fromEnum c2) :: Color
+
+main = do
+  print $ mix Red Blue
+  print $ mix Red $ mix Blue Green
